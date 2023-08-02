5d6b3d03e2d1b774bdd8bad350754c92372a59b1
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-08-02 11:02:35 -0300

    Passar eventos por props - #11

--- my-project/src/components/Event.tsx
+++ my-project/src/components/Event.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Button } from "./event/Button.tsx";
 
 export const Event: React.FC<{num: number}> = ({num}) => {
   const myEvent = () => {
     console.log(`Fui ativado: ${num}`)
   }
 
+  const secondEvent = () => {
+    console.log('Ativando o segundo evento')
+  }
+
   return (
     <>
       <p>Clique para disparar um evento</p>
       <button onClick={myEvent}>Ativar</button>
+      <Button click={myEvent}>Primeiro evento</Button>
+      <Button click={secondEvent}>Segundo evento</Button>
     </>
   )
 }
+
