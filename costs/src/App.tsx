f0cde8ea4756cccb2b81774565e377502a5f65ed
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-09-08 09:17:05 -0300

    Estruturando o projeto - #18

--- costs/src/App.tsx
+++ costs/src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Home } from './components/pages/Home.tsx'
+import { Company } from './components/pages/Company.tsx'
+import { Contact } from './components/pages/Contact.tsx'
+import { NewProject } from './components/pages/NewProject.tsx'
+import { Container } from './components/layout/Container.tsx'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <ul>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/company'}>Empresa</Link></li>
+        <li><Link to={'/contact'}>Contato</Link></li>
+        <li><Link to={'/newproject'}>Novo projeto</Link></li>
+      </ul>
+      <Container customClass="min_height">
+        <Routes> 
+          <Route exact path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />   
+        </Routes>
+      </Container>
+      <p>footer</p>
+    </Router>
   )
 }
