51ce6f788efe5cdcabafe25f019f9be11155661e
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-09-15 09:48:12 -0300

    Criando a home - #20

--- costs/src/components/pages/Home.tsx
+++ costs/src/components/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import { LinkButton } from '../layout/LinkButton.tsx'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <main className={styles.home}>
+      <h1>Bem-vindo ao <span><strong>Costs</strong></span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject">Criar projeto</LinkButton>
+      <img src={savings} alt="Costs" />
+    </main>
   )
 }
