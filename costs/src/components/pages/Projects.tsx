ea5aca538a5d07dea378ffcc3aff0bc6deddcd75
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-09-19 11:18:30 -0300

    Criando Dashboard - #26

--- costs/src/components/pages/Projects.tsx
+++ costs/src/components/pages/Projects.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { Messages } from '../layout/Messages.tsx'
+import { Container } from '../layout/Container.tsx'
+import { LinkButton } from '../layout/LinkButton.tsx'
+import styles from './Projects.module.css'
 
 export const Projects = () => {
   const location = useLocation()
@@ -10,9 +13,15 @@ export const Projects = () => {
     message = location.state.message
 
   return (
-    <div>
+    <div className={styles.project_container}>
       {message && <Messages type="success" msg={message} />}
-      <h1>Meus Projetos</h1>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject">Criar Projeto</LinkButton>
+      </div>
+      <Container customClass="start">
+        <p>projetos...</p>
+      </Container>
     </div>
   )
 }
